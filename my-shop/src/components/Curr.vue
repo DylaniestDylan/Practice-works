5c272e9cd5b048c7041ab73038eaf59dd6f193fe
DylaniestDylan <joonas.sulkakoski@gmail.com>
2022-09-01 12:19:39 +0300

    Commit this quickly while it still works holy hell

---
+++ my-shop/src/components/Curr.vue
@@ -0,0 +1,17 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<template>
+  {{ dollar(amt) }}
+</template>
+
+<script>
+export default {
+  props: ['amt'],
+  methods: {
+    dollar(value) {
+      return '$' + Number.parseFloat(value).toFixed(2)
+    }
+  }
+}
+</script>
+
+<style></style>
